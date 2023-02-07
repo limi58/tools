4765540737bac18671155021120f377795f9d9fd
limi58 <limi58@foxmail.com>
2023-02-07 15:10:54 +0800

    Update get_files.ts

--- src/get_files.ts
+++ src/get_files.ts
@@ -1,51 +1,57 @@
-import path from 'path'
-import fs from 'fs'
-
-
-interface IProps{
-  pathname: string, // 目录路径
-  excludeExp?: RegExp, // 需要在递归中排除的文件夹或文件名
-  filterExp?: RegExp, // 得到文件列表后，进一步筛选出的文件或文件夹名称
-  deep?: boolean, // 是否递归获取
+interface GetFilesProps {
+  /** 要查找的目录路径 */
+  pathname: string
+  /** 需要在递归中排除的文件夹或文件名，和 includeExp的 不能共存 */
+  excludeExp?: RegExp
+  /** 需要在递归中包含的文件夹或文件名，和 excludeExp 不能共存 */
+  includeExp?: RegExp
+  /** 是否递归获取，否则只查找第一层 */
+  deep?: boolean
 }
 
 /**
  * 获取目标文件夹下的指定文件
  * 返回文件绝对路径列表
+ * @example
+ * const files = getFiles({
+ *   pathname: path.resolve(process.cwd(), 'pages'),
+ *   deep: true,
+ *   excludeExp: /withdraw/,
+ * })
  */
-function getFiles(props: IProps) {
+export function getFiles(props: GetFilesProps) {
   const files: Array<string> = []
-  const filterExp = props.filterExp
+  if ('excludeExp' in props && 'includeExp' in props) {
+    throw new Error('can not both set excludeExp and includeExp')
+  }
   getAllFilesInner(props)
 
-  function getAllFilesInner(innerProps: IProps) {
-    const excludeExp = innerProps.excludeExp
-    const pathname = innerProps.pathname
-    const dirs = fs.readdirSync(pathname)
-    const filterDirs = dirs.filter(item => {
-      if (!excludeExp) {
-        return true
-      }
-      return !excludeExp.test(item) 
-    })
-    const realDirs = filterDirs.map(item => path.join(pathname, item))
-    realDirs.forEach(item => {
+  function getAllFilesInner(innerProps: GetFilesProps) {
+    // all files and dirs
+    const dirs = fs.readdirSync(innerProps.pathname)
+    let filterDirs: string[] = []
+    if ('excludeExp' in props) {
+      filterDirs = dirs.filter((item) => !props.excludeExp?.test(item))
+    } else if ('includeExp' in props) {
+      filterDirs = dirs.filter((item) => props.includeExp?.test(item))
+    } else {
+      filterDirs = dirs
+    }
+    const realDirs = filterDirs.map((item) =>
+      path.join(innerProps.pathname, item),
+    )
+    realDirs.forEach((item) => {
       if (fs.statSync(item).isDirectory()) {
-        if (innerProps.deep) {
+        if (props.deep) {
           getAllFilesInner({
-            excludeExp: innerProps.excludeExp,
+            ...innerProps,
             pathname: item,
-          }) 
+          })
         }
       } else {
         files.push(item)
       }
     })
   }
-  if (!filterExp) {
-    return files
-  }
-  return files.filter(item => filterExp.test(item))
+  return files
 }
-
-export default getFiles
