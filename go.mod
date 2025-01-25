9ba48c181da6615804bf28996f891b617bbadb52
limi-ubuntu <limi58@foxmail.com>
2025-01-26 01:04:04 +0800

    feat(heic): use gorouting

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module tools
 
 go 1.23.2
+
+require (
+	github.com/samber/lo v1.47.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- heic/index.go
+++ heic/index.go
@@ -7,12 +7,17 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"tools/utils"
+
+	"github.com/samber/lo"
 )
 
-const ext = "heic"
-const skipKb = 250
+const TARGET_EXT = "heic"
+const SKIP_KB = 250
+
+var HANDLE_EXT = []string{"jpg", "jpeg", "png", "heic"}
 
 func Main() {
 	var dir string
@@ -20,7 +25,7 @@ func Main() {
 	fmt.Scanln(&dir)
 	timeStart := time.Now()
 	// 创建输出目录
-	targetDir := filepath.Join(dir, ext)
+	targetDir := filepath.Join(dir, TARGET_EXT)
 	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
 		cmd := exec.Command("mkdir", targetDir)
 		_, err := cmd.Output()
@@ -43,7 +48,7 @@ func Main() {
 func getFileNameWithoutExt(path string) string {
 	// 获取文件名（包含后缀）
 	fileNameWithExt := filepath.Base(path)
-	// 获取文件后缀
+	// 获取文件后缀，如 .png
 	ext := filepath.Ext(fileNameWithExt)
 	// 去除后缀
 	fileName := strings.TrimSuffix(fileNameWithExt, ext)
@@ -52,24 +57,36 @@ func getFileNameWithoutExt(path string) string {
 
 func handleFile(file *utils.FileItem, targetDir string) error {
 	fileName := file.Info.Name()
+	ext := filepath.Ext(file.Info.Name())
+	ext = lo.Substring(ext, 1, uint(utf8.RuneCountInString(ext)))
+	ext = strings.ToLower(ext)
+
+	// 过滤文件，只允许图片被处理
+	if !lo.Contains(HANDLE_EXT, ext) {
+		fmt.Println("no allow ext file:", fileName)
+		return nil
+	}
 
 	// 图片太小，直接跳过
-	if file.Info.Size()/1000 <= skipKb {
+	if file.Info.Size()/1000 <= SKIP_KB {
 		fmt.Println("图片太小，直接跳过", fileName)
 		return nil
 	}
 
 	// 开始处理
-	targetFilePath := filepath.Join(targetDir, getFileNameWithoutExt(file.Path)+"."+ext)
-	cmd := exec.Command("vips", "heifsave", file.Path, targetFilePath, "--Q=50", "--effort=9")
+	go func() {
+		targetFilePath := filepath.Join(targetDir, getFileNameWithoutExt(file.Path)+"."+TARGET_EXT)
+		cmd := exec.Command("vips", "heifsave", file.Path, targetFilePath, "--Q=50", "--effort=9")
 
-	_, err := cmd.Output()
+		_, err := cmd.Output()
 
-	if err != nil {
-		fmt.Println(err)
-	}
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	fmt.Println("已完成", fileName)
+		fmt.Println("已完成", fileName)
+
+	}()
 
 	return nil
 }
