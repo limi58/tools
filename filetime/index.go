b4fa0d676401a8b72d88c1b7e5355a65ce193cf3
limi58 <limi58@foxmail.com>
2025-04-21 00:46:03 +0800

    fix(filetime): run on mac and linux (#16)

--- filetime/index.go
+++ filetime/index.go
@@ -4,27 +4,48 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"syscall"
 	"time"
 	"tools/utils"
 )
 
 // 获取文件创建时间，兼容多个操作系统
-func getFileCt(filePath string) string {
+func getFileCt(filePath string) (string, error) {
 	fileInfo, err := os.Stat(filePath)
 	if err != nil {
-		utils.ExitIfErr(err)
+		return "", fmt.Errorf("获取文件信息失败: %w", err)
 	}
 
-	stat := fileInfo.Sys().(*syscall.Stat_t)
-	var createTime time.Time
-	if stat.Birthtimespec.Sec != 0 {
-		createTime = time.Unix(stat.Birthtimespec.Sec, stat.Birthtimespec.Nsec)
-	} else {
-		createTime = time.Unix(stat.Ctimespec.Sec, stat.Ctimespec.Nsec)
+	// 从 FileInfo 中获取底层的系统特定信息
+	sysStat := fileInfo.Sys()
+	if sysStat == nil {
+		// 这种情况理论上不应该在 Linux/macOS 上发生，但作为健壮性检查
+		return "", fmt.Errorf("无法获取系统特定文件信息")
 	}
 
-	return createTime.Format(time.DateOnly)
+	// 将其类型断言为 *syscall.Stat_t
+	stat, ok := sysStat.(*syscall.Stat_t)
+	if !ok {
+		return "", fmt.Errorf("系统文件信息类型断言失败")
+	}
+
+	var creationTime time.Time
+	// 根据操作系统选择时间戳
+	switch runtime.GOOS {
+	case "darwin": // macOS
+		// macOS 提供 Birthtimespec，如果在 macOS 上编译就取消注释下面
+		// ts := stat.Birthtimespec
+		// creationTime = time.Unix(ts.Sec, ts.Nsec)
+	case "linux":
+		// !! 这不是真正的创建时间！ Linux 标准 stat 不提供 Birthtime，回退到 Ctim (状态更改时间)
+		ts := stat.Ctim
+		creationTime = time.Unix(ts.Sec, ts.Nsec)
+	default:
+		return "", fmt.Errorf("当前操作系统 (%s) 不支持此方法获取创建时间", runtime.GOOS)
+	}
+
+	return creationTime.Format(time.DateOnly), nil
 }
 
 func Main() {
@@ -36,10 +57,13 @@ func Main() {
 		Dir:         dir,
 		IsRecursion: false,
 		Cb: func(file *utils.FileItem) error {
-			timeStr := getFileCt(file.Path)
+			timeStr, err := getFileCt(file.Path)
+			if err != nil {
+				return nil
+			}
 			newFileName := fmt.Sprintf("%s-%s", timeStr, utils.GetRanStr(4)) + filepath.Ext(file.Path)
 			newFilePath := filepath.Join(dir, newFileName)
-			err := os.Rename(file.Path, newFilePath)
+			err = os.Rename(file.Path, newFilePath)
 			if err != nil {
 				fmt.Println(err)
 			}
